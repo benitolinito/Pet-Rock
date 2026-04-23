13733b390aa8bade6ad3672b4cf8b16b696e6119
Ben Lin <blin97198@gmail.com>
2026-04-23 19:48:19 -0400

    added basic ui for signup

--- lib/phone.ts
+++ lib/phone.ts
@@ -1,6 +1,12 @@
-import { parsePhoneNumberFromString } from "libphonenumber-js";
+import {
+  parsePhoneNumberFromString,
+  type CountryCode,
+} from "libphonenumber-js";
 
-export function normalizePhoneNumber(input: string, defaultCountry = "US") {
+export function normalizePhoneNumber(
+  input: string,
+  defaultCountry: CountryCode = "US",
+) {
   const parsed = parsePhoneNumberFromString(input, defaultCountry);
 
   if (!parsed?.isValid()) {
